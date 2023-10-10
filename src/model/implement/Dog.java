ca7a32c559cd18ea3717d08674c36052164312e5
ednsk <119007272+ednsk@users.noreply.github.com>
2023-10-11 00:59:21 +0700

    Upload animals

--- src/model/implement/Dog.java
+++ src/model/implement/Dog.java
@@ -5,9 +5,9 @@ import com.rodini.kennel.model.AnimalGenius;
 
 import java.time.LocalDate;
 
-public class Cat extends AbstractPet {
-    public Cat(String name, LocalDate birthDate) {
+public class Dog extends AbstractPet {
+    public Dog(String name, LocalDate birthDate) {
         super(name, birthDate);
-        setAnimalGenius(AnimalGenius.CAT);
+        setAnimalGenius(AnimalGenius.DOG);
     }
 }
--- src/model/implement/Hamster.java
+++ src/model/implement/Hamster.java
@@ -5,9 +5,9 @@ import com.rodini.kennel.model.AnimalGenius;
 
 import java.time.LocalDate;
 
-public class Cat extends AbstractPet {
-    public Cat(String name, LocalDate birthDate) {
+public class Hamster extends AbstractPet {
+    public Hamster(String name, LocalDate birthDate) {
         super(name, birthDate);
-        setAnimalGenius(AnimalGenius.CAT);
+        setAnimalGenius(AnimalGenius.HAMSTER);
     }
 }
